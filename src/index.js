fceb57874f737ab6ec85cf59c666190365083e77
frarosset <37275581+frarosset@users.noreply.github.com>
2024-09-16 18:22:27 +0200

    Add getUnit module
    This modules exports getUnit() function that can be used to
    retrieve the correct unit measure for a weather property based on
    the provided unitGroup.
    
    Note that the properties are named based on the data retrieved
    from the visual crossing API.
    
    See:
    - https://www.visualcrossing.com/resources/documentation/weather-data/weather-data-documentation/
    - https://www.visualcrossing.com/resources/documentation/weather-api/unit-groups-and-measurement-units/

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./index.css";
 import { getApiUrl, getApiUrlBasic } from "./js-modules/getApiUrl.js";
+import getUnit from "./js-modules/getUnit.js";
 
 // temporary code
 console.log(getApiUrl("Rome"));
 console.log(getApiUrlBasic("New York"));
+
+console.log(getUnit("temp", "base"));
+console.log(getUnit("visibility", "us"));
