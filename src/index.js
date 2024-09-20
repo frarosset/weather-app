cabac21d465653619b23e811174ffcc9d2e33889
frarosset <37275581+frarosset@users.noreply.github.com>
2024-09-20 10:48:22 +0200

    Setup PubSub to display the weather data when needed
    PubSub has been used to trigger the rendering of valid weather
    results. The subscription to the "RENDER WEATHER DATA" token has
    been temporarly done in the index.js function, although that part
    needs to be rearranged. When this token is published, the
    renderWeatherDataPage function gets properly called.
    
    Note that in the renderSelectLocationPage, a "RENDER WEATHER DATA"
    token is published, along with the fetched data, when this needs
    to be displayed.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import "./index.css";
 import renderSelectLocationPage from "./js-modules/dom/renderSelectLocationPage.js";
+import renderWeatherDataPage from "./js-modules/dom/renderWeatherDataPage.js";
+import PubSub from "pubsub-js";
 // temporary code
 
 renderSelectLocationPage(document.body);
+
+PubSub.subscribe("RENDER WEATHER DATA", (msg, data) => {
+  console.log(msg, `[${data.current.datetime}, ${data.location}]`);
+  renderWeatherDataPage(document.body, data);
+});
