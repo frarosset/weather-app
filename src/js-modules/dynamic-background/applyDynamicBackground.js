43267e89f88d3c4d0729adc7fe7089f8695c9f8d
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-01 16:59:16 +0200

    Add testDynamicBackground() function to applyDynamicBackground
    The function generate a div containing the gradients for the entire day,
    every some minutes, in the current weather conditions, highlighting the
    current hour (with an arrow), and telling apart night and day by text
    color. The div is appended to the end of the body.

--- src/js-modules/dynamic-background/applyDynamicBackground.js
+++ src/js-modules/dynamic-background/applyDynamicBackground.js
@@ -5,9 +5,10 @@ import { adjustRgbGradientBasedOnWeather } from "./adjustBasedOnWeather.js";
 export default function applyDynamicBackground(propertyName, data) {
   const background = computeDynamicBackground(data);
   document.documentElement.style.setProperty(propertyName, background);
+  testDynamicBackground(data);
 }
 
-function computeDynamicBackground(data) {
+export function computeDynamicBackground(data) {
   // Get the background color assuming weather conditions
   const rgbGradient = getCurrentRgbGradient(data.current);
 
@@ -23,4 +24,67 @@ function computeDynamicBackground(data) {
   );
 
   return getCssGradient(adjustedRgbGradient);
+}
+
+// test function
+function testDynamicBackground(data, deltaM = 15) {
+  // save current datetimeMin (it will be modified)
+  const oldCurrentDatetimeMin = data.current.datetimeMin;
+
+  const SR = data.current.sunriseMin;
+  const SS = data.current.sunsetMin;
+
+  // just a function to display time
+  const minutesToHHMM = (m) => {
+    const [hh, mm] = [Math.floor(m / 60) % 24, m % 60];
+    return `${hh < 10 ? "0" : ""}${hh}:${mm < 10 ? "0" : ""}${mm}`;
+  };
+
+  const container = document.createElement("div");
+  const className = "test-dynamic-background";
+  container.classList.add(className);
+  container.style.display = "flex";
+  container.style.flexWrap = "wrap";
+
+  // show a gradient every deltaM minutes, from modnight to midnight
+  for (let m = 0; m < 1440; m += deltaM) {
+    data.current.datetimeMin = m;
+    const background = computeDynamicBackground(data);
+
+    const div = document.createElement("div");
+
+    div.innerText = minutesToHHMM(m);
+    // highlight current time interval
+    if (oldCurrentDatetimeMin >= m && oldCurrentDatetimeMin < m + deltaM) {
+      div.innerText += ` <---`;
+    }
+
+    div.style.width = "min-content";
+    div.style.height = "120px";
+    div.style.margin = "2px 0px";
+    div.style.writingMode = "vertical-rl";
+    div.style.background = background;
+    div.style.lineHeight = 1.5;
+    if (m < SR || m > SS) {
+      // nighttime
+      div.style.color = "white";
+    } else {
+      //daytime
+      div.style.color = "black";
+      div.style.fontWeight = "bold";
+    }
+
+    container.appendChild(div);
+  }
+
+  // if the div is already present, replace it
+  const existingContainer = document.querySelector(`.${className}`);
+  if (existingContainer != null) {
+    existingContainer.replaceWith(container);
+  } else {
+    document.body.appendChild(container);
+  }
+
+  // restore current datetimeMin (it will be modified)
+  data.current.datetimeMin = oldCurrentDatetimeMin;
 }
