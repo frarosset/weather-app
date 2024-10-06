7a7461cc2b7ac859bacfdb19f6340383b64a3c36
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-06 11:28:19 +0200

    Move initialization code in ./js-modules/initWebpage.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,4 @@
 import "./index.css";
-import renderSelectLocationPage from "./js-modules/dom/renderSelectLocationPage.js";
-import renderWeatherDataPage from "./js-modules/dom/renderWeatherDataPage.js";
-import renderErrorPage from "./js-modules/dom/renderErrorPage.js";
-import renderFetchingDataPage from "./js-modules/dom/renderFetchingDataPage.js";
-import PubSub from "pubsub-js";
-// temporary code
-import { freezeAllAnimations } from "./js-modules/dom/animations.js";
+import initWebpage from "./js-modules/initWebpage.js";
 
-renderSelectLocationPage(document.body);
-
-const resultsDiv = document.createElement("div");
-document.body.appendChild(resultsDiv);
-
-PubSub.subscribe("RENDER WEATHER DATA", (msg, data) => {
-  console.log(
-    msg,
-    `[${data.formatTz(data.current.datetime, "EEE, d MMM yyyy, HH:mm")}, ${data.location}]`
-  );
-  renderWeatherDataPage(resultsDiv, data);
-});
-
-PubSub.subscribe("RENDER ERROR PAGE", (msg, error) => {
-  console.log(msg, `[${error.message}]`);
-  renderErrorPage(resultsDiv, error);
-});
-
-PubSub.subscribe("RENDER FETCHING DATA", (msg, locationStr) => {
-  console.log(msg, `[${locationStr}]`);
-  renderFetchingDataPage(resultsDiv, locationStr);
-});
-
-freezeAllAnimations();
+initWebpage();
---
+++ src/js-modules/initWebpage.js
@@ -0,0 +1,34 @@
+import renderSelectLocationPage from "./dom/renderSelectLocationPage.js";
+import renderWeatherDataPage from "./dom/renderWeatherDataPage.js";
+import renderErrorPage from "./dom/renderErrorPage.js";
+import renderFetchingDataPage from "./dom/renderFetchingDataPage.js";
+import PubSub from "pubsub-js";
+// temporary code
+import { freezeAllAnimations } from "./dom/animations.js";
+
+export default function initWebpage() {
+  renderSelectLocationPage(document.body);
+
+  const resultsDiv = document.createElement("div");
+  document.body.appendChild(resultsDiv);
+
+  PubSub.subscribe("RENDER WEATHER DATA", (msg, data) => {
+    console.log(
+      msg,
+      `[${data.formatTz(data.current.datetime, "EEE, d MMM yyyy, HH:mm")}, ${data.location}]`
+    );
+    renderWeatherDataPage(resultsDiv, data);
+  });
+
+  PubSub.subscribe("RENDER ERROR PAGE", (msg, error) => {
+    console.log(msg, `[${error.message}]`);
+    renderErrorPage(resultsDiv, error);
+  });
+
+  PubSub.subscribe("RENDER FETCHING DATA", (msg, locationStr) => {
+    console.log(msg, `[${locationStr}]`);
+    renderFetchingDataPage(resultsDiv, locationStr);
+  });
+
+  freezeAllAnimations();
+}
