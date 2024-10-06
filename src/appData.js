7fbfc2297be88743cd9bc023d493083da5f0744c
frarosset <frarosset>
2024-10-06 12:12:22 +0200

    Add functions to get/set/reset/show weather data of home location
    The weather data is shown only if the home location is not null.
    
    To test the code, the home location has been temporarily set to
    'London' in the initWebpage() function, and then the corresponding
    weather data are shown.

--- src/appData.js
+++ src/appData.js
@@ -1,7 +1,30 @@
-export const appData = {
+import { showWeatherDataFor } from "./js-modules/showWeatherDataFor.js";
+
+const appData = {
   location: {
-    home: "",
+    home: null,
     bookmarked: [],
     last: "",
   },
 };
+
+// Functions related to location
+
+export function setHomeLocation(str) {
+  appData.location.home = str;
+}
+
+export function getHomeLocation() {
+  return appData.location.home;
+}
+
+export function showHomeLocation() {
+  const homeLocation = appData.location.home;
+  if (homeLocation !== null) {
+    showWeatherDataFor(homeLocation);
+  }
+}
+
+export function resetHomeLocation() {
+  appData.location.home = null;
+}
