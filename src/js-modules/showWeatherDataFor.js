9d81b945466f2d759d99dd22f2ed9ac28e056a3d
frarosset <frarosset>
2024-10-07 11:15:54 +0200

    Add functions to get/set/reset/show weather data of last location
    The last location is set when the showWeatherDataFor is called.

--- src/js-modules/showWeatherDataFor.js
+++ src/js-modules/showWeatherDataFor.js
@@ -1,11 +1,16 @@
 import PubSub from "pubsub-js";
 import getData from "./api-dependant/getData.js";
+import { setLastLocation } from "../appData.js";
 
 export function showWeatherDataFor(location) {
   PubSub.publish("RENDER FETCHING DATA", location);
   return getData(location)
     .then((dataObj) => {
       PubSub.publish("RENDER WEATHER DATA", dataObj);
+      return dataObj;
+    })
+    .then((dataObj) => {
+      setLastLocation(dataObj.location);
     })
     .catch((errorObj) => {
       PubSub.publish("RENDER ERROR PAGE", errorObj);
