6e1cae336bceb065066a2aeaa1e3b4c500094b1d
frarosset <frarosset>
2024-10-07 17:14:12 +0200

    Add remove button to home location in select location view

--- src/js-modules/dom/renderSelectLocationPage.js
+++ src/js-modules/dom/renderSelectLocationPage.js
@@ -5,14 +5,21 @@ import {
 } from "../../js-utilities/commonDomComponents.js";
 import { resetContent } from "../../js-utilities/commonDomUtilities.js";
 import { showWeatherDataFor } from "../showWeatherDataFor.js";
-import { showHomeLocation, getBookmarkedLocations } from "../../appData.js";
+import {
+  showHomeLocation,
+  getBookmarkedLocations,
+  getHomeLocation,
+} from "../../appData.js";
+import { setAnimation, icons, forcePlayAnimation } from "./animations.js";
+import { resetHomeLocation } from "../../appData.js";
 
 const blockName = "select-location-page";
 const cssClass = {
   searchDiv: "search-div",
   searchInput: "search-input",
   homeDiv: "home-div",
-  homeButton: "home-button",
+  homeButton: "home-btn",
+  removeHomeButton: "remove-home-btn",
   bookmarkedDiv: "bookmarked-div",
   bookmarkedList: "bookmarked-list",
   bookmarkedBtn: "bookmarked-btn",
@@ -68,16 +75,46 @@ function initSearchDiv() {
 function initHomeLocationDiv() {
   const div = initDiv(getCssClass("homeDiv"));
 
-  const btn = initButton(
-    getCssClass("homeButton"),
-    showHomeLocation,
-    null,
-    "Home"
-  );
+  const location = getHomeLocation();
+  if (location !== null) {
+    const removeBtn = initRemoveHomeButton(div);
 
-  div.append(btn);
+    const btn = initButton(
+      getCssClass("homeButton"),
+      showHomeLocation,
+      null,
+      location
+    );
+
+    div.append(removeBtn, btn);
+  }
 
   return div;
+}
+
+function initRemoveHomeButton(parentDiv) {
+  const removeBtnCallback = () => {
+    forcePlayAnimation(animation, -1);
+    resetHomeLocation();
+    resetContent(parentDiv);
+  };
+
+  const removeBtn = initButton(
+    getCssClass("removeHomeButton"),
+    removeBtnCallback
+  );
+  const animation = setAnimation(removeBtn, icons.heart, false, false);
+  forcePlayAnimation(animation, 1);
+
+  removeBtn.addEventListener("mouseenter", () => {
+    forcePlayAnimation(animation, -1);
+  });
+
+  removeBtn.addEventListener("mouseleave", () => {
+    forcePlayAnimation(animation, 1);
+  });
+
+  return removeBtn;
 }
 
 function initBookmarkedLocationDiv() {
