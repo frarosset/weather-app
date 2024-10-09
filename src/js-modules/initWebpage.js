6bdc520728dee99be695bebb9ece5e56a94de69e
frarosset <37275581+frarosset@users.noreply.github.com>
2024-10-09 09:12:35 +0200

    Use main element as page container instead of generic div

--- src/js-modules/initWebpage.js
+++ src/js-modules/initWebpage.js
@@ -10,12 +10,12 @@ import PubSub from "pubsub-js";
 export default function initWebpage() {
   initAppData();
 
-  const resultsDiv = document.createElement("div");
-  document.body.appendChild(resultsDiv);
+  const main = document.createElement("main");
+  document.body.appendChild(main);
 
   PubSub.subscribe("RENDER SELECT LOCATION DATA", (msg) => {
     console.log(msg);
-    renderSelectLocationPage(resultsDiv);
+    renderSelectLocationPage(main);
   });
 
   PubSub.subscribe("RENDER WEATHER DATA", (msg, data) => {
@@ -23,28 +23,28 @@ export default function initWebpage() {
       msg,
       `[${data.formatTz(data.current.datetime, "EEE, d MMM yyyy, HH:mm")}, ${data.location}]`
     );
-    renderWeatherDataPage(resultsDiv, data);
+    renderWeatherDataPage(main, data);
   });
 
   PubSub.subscribe("RENDER ERROR PAGE", (msg, error) => {
     console.log(msg, `[${error.message}]`);
-    renderErrorPage(resultsDiv, error);
+    renderErrorPage(main, error);
   });
 
   PubSub.subscribe("RENDER FETCHING DATA", (msg, locationStr) => {
     console.log(msg, `[${locationStr}]`);
-    renderFetchingDataPage(resultsDiv, locationStr);
+    renderFetchingDataPage(main, locationStr);
   });
 
   PubSub.subscribe("RENDER LOCATING", (msg) => {
     console.log(msg);
-    renderLocatingPage(resultsDiv);
+    renderLocatingPage(main);
   });
 
   PubSub.subscribe("RENDER SETTINGS", (msg) => {
     console.log(msg);
-    renderSettingsPage(resultsDiv);
+    renderSettingsPage(main);
   });
 
-  if (!showHomeLocation()) renderSelectLocationPage(resultsDiv);
+  if (!showHomeLocation()) renderSelectLocationPage(main);
 }
