f98d3438e5f9c75ec09c8b2cd87d9e6717825ff9
frarosset <frarosset>
2024-10-09 10:20:07 +0200

    Add credit footer

--- src/js-modules/initWebpage.js
+++ src/js-modules/initWebpage.js
@@ -6,16 +6,25 @@ import renderLocatingPage from "./dom/renderLocatingPage.js";
 import renderSettingsPage from "./dom/renderSettingsPage.js";
 import { initAppData, showHomeLocation } from "../appData.js";
 import PubSub from "pubsub-js";
+import setCreditFooter from "../js-utilities/creditFooter.js";
 
 export default function initWebpage() {
   initAppData();
 
   const main = document.createElement("main");
-  document.body.appendChild(main);
+  subscribeToRender(main);
 
+  document.body.appendChild(main);
+  if (!showHomeLocation()) renderSelectLocationPage(main);
+
+  // Add credit footer at the bottom
+  setCreditFooter();
+}
+
+function subscribeToRender(div) {
   PubSub.subscribe("RENDER SELECT LOCATION DATA", (msg) => {
     console.log(msg);
-    renderSelectLocationPage(main);
+    renderSelectLocationPage(div);
   });
 
   PubSub.subscribe("RENDER WEATHER DATA", (msg, data) => {
@@ -23,28 +32,26 @@ export default function initWebpage() {
       msg,
       `[${data.formatTz(data.current.datetime, "EEE, d MMM yyyy, HH:mm")}, ${data.location}]`
     );
-    renderWeatherDataPage(main, data);
+    renderWeatherDataPage(div, data);
   });
 
   PubSub.subscribe("RENDER ERROR PAGE", (msg, error) => {
     console.log(msg, `[${error.message}]`);
-    renderErrorPage(main, error);
+    renderErrorPage(div, error);
   });
 
   PubSub.subscribe("RENDER FETCHING DATA", (msg, locationStr) => {
     console.log(msg, `[${locationStr}]`);
-    renderFetchingDataPage(main, locationStr);
+    renderFetchingDataPage(div, locationStr);
   });
 
   PubSub.subscribe("RENDER LOCATING", (msg) => {
     console.log(msg);
-    renderLocatingPage(main);
+    renderLocatingPage(div);
   });
 
   PubSub.subscribe("RENDER SETTINGS", (msg) => {
     console.log(msg);
-    renderSettingsPage(main);
+    renderSettingsPage(div);
   });
-
-  if (!showHomeLocation()) renderSelectLocationPage(main);
 }
