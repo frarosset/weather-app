e913bd9145a4096d43b967d7b2a053450129f9da
frarosset <frarosset>
2024-10-10 21:39:43 +0200

    Move the code to generate the weather insight div to initWeatherDataWeatherInsightDiv module

---
+++ src/js-modules/dom/initWeatherDataWeatherInsightDiv.js
@@ -0,0 +1,36 @@
+import {
+  initDiv,
+  initH3,
+  initP,
+} from "../../js-utilities/commonDomComponents.js";
+
+const blockName = "weather-data-page";
+const cssClass = {
+  weatherInsightDiv: "weather-insight-div",
+  weatherInsightH3: "weather-insight-h3",
+  weatherInsightP: "weather-insight-p",
+};
+const getCssClass = (element) => `${blockName}__${cssClass[element]}`;
+
+// Subdata might be, eg, data.days
+
+export function initWeatherDataWeatherInsightDiv(str) {
+  const div = initWeatherInsightDiv(str, "");
+  return div;
+}
+
+function initWeatherInsightDiv(str) {
+  const div = initDiv(getCssClass("weatherInsightDiv"));
+
+  const weatherInsightH3 = initH3(
+    getCssClass("weatherInsightH3"),
+    null,
+    "Weather insight"
+  );
+
+  const weatherInsightP = initP(getCssClass("weatherInsightP"), null, str);
+
+  div.append(weatherInsightH3, weatherInsightP);
+
+  return div;
+}
